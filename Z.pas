083014b6541b7ee0f838f0f8811303a5a6814e5c
@Konstantine <57166789+mind-protector@users.noreply.github.com>
2020-11-20 15:36:55 +0500

    Update Z.pas

--- Z.pas
+++ Z.pas
@@ -1,8 +1,8 @@
-﻿program Z;
+program Z;
   uses graphABC;
   const
     H = 480;
-    W = 640;
+    W = 600;
     
     X1 = W div 2;
     Y1 = H div 2;
@@ -28,35 +28,46 @@
   (
     X, // X-axis of the centre of the base
     Y, // Y-axis of the centre of the base
-    { @par optional } top,
+    { @par optional 1 } top,
     height,
-    base,
+    { @par optional 2 }base,
     outlineWidth: integer;
-    { @par optional } angle: real; // equal angles at the base
+    { @par optional 2 } angle: real; // equal angles at the base
     firstColor, // outline color
     secondColor: System.Drawing.Color // main color
   );
   {
-    Drows isosceles trapezoid by required parameters such as X, Y, height, base.
-    You need push one of the optional parameters such as angle or base.
+    Drows isosceles trapezoid by required parameters such as X, Y, height.
+    You need push one of the optional parameters such as angle and base or top.
     Push 0 to ignore an optional parameter.
   }
   var
     lX, rX, ToplX, ToprX, TopY, k: integer;
+    alfa: real;
     { @var k means distance from edges of the base and top }
+    { @var alfa is angle in RAD }
   begin
-    { edges of the base }
-    lX := X - base div 2;
-    rX := X + base div 2;
     
+    alfa := angle / RAD;
     TopY := Y - height;
     
     { calculating k using pushed optional parameter }
     if angle = 0 then
       k := (base - top) div 2
     else
-      k := round(height / tan(angle / RAD));
+    begin
+      if angle > 90 then alfa := alfa - pi;
+      if round(angle) mod 90 = 0 then k := 0
+      else k := round(height / (sin(alfa) / cos(alfa)))
+    end;
     
+    { 
+      Calculating edges of the base using pushed optional parameter.
+      If you ignore base it will be equal to 2k.
+    }
+    if (base = 0) and (angle <= 90) then base := 2*k;
+    lX := X - base div 2;
+    rX := X + base div 2;
     
     { edges of the top }
     ToplX := lX + k;
@@ -77,6 +88,6 @@ begin
     SetWindowSize(W, H);
     ClearWindow(winColor);
     
-    Trapezoid(X1, Y1, 150, 100, 100, 4, 0, clBlack, clRed);
+    Trapezoid(X1, Y1, 0, -100, -40, 4, 140, clBlack, clRed);
 end.
     
